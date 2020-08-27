cca8b491e3ff68efcc1cc04a95f4beb4ef9f44a2
Richard Axornam <axornam.richard@gmail.com>
2020-08-27 11:07:10 +0000

    deploying to heroku

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: "mongodb://root:root@localhost:27017/digipharm",
+    mongoURI: "mongodb://root:root@localhost:27017/shopping_list",
 };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const items = require("./routes/api/users");
+// const items = require("./routes/api/users");
 
 const app = express();
 
@@ -23,13 +23,16 @@ mongoose
     .catch((err) => console.log(err));
 
 // Use Routes
-app.use("/api/users", items);
-
-const port = process.env.PORT || 5000;
+// app.use("/api/users", items);
 
 // step 3
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build/"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "/client", "build", "index.html"));
+    });
 }
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log("Server started on : " + port));
