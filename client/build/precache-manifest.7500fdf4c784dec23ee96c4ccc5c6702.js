2e25d9f3096bc228f7f8f104d936b69e83a80751
Richard Axornam <axornam.richard@gmail.com>
2020-09-05 02:59:58 +0000

    run production build

---
+++ client/build/precache-manifest.7500fdf4c784dec23ee96c4ccc5c6702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327a160f10b65cb1eb8874e23f3cfb16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1db3e0f6f991c8637c8",
+    "url": "/static/css/main.40fa5a8e.chunk.css"
+  },
+  {
+    "revision": "24675ca94e438b6131f3",
+    "url": "/static/js/2.4ff95a43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ff95a43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1db3e0f6f991c8637c8",
+    "url": "/static/js/main.12024045.chunk.js"
+  },
+  {
+    "revision": "1354688a4eb83a6768bd",
+    "url": "/static/js/runtime-main.fb7776c8.js"
+  }
+]);
